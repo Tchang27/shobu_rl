improved model design, fixed value model loss calculation

eval.py
@@ -114,7 +114,7 @@ def round_robin(
 	rounds = [[(rated_agents, max_moves_per_game, p[0], p[1]) for p in product(range(num_players), repeat=2) if p[0] != p[1]]] * num_rounds
 	win_matrix = np.zeros((num_players, num_players))
 
-	with Pool(16) as p:
+	with Pool(8) as p:
 		for round in tqdm(rounds):
 			random.shuffle(round)  # this may be good or bad idea
 			num_decisive = 0

models.py
@@ -28,12 +28,10 @@ class ResidualBlock2D(nn.Module):
     
 
 class MLP_Head(nn.Module):
-    def __init__(self, in_channels, out_channels, hidden_channels=256, dropout_rate=0.3):
+    def __init__(self, in_channels, out_channels, hidden_channels=128, dropout_rate=0.3):
         super(MLP_Head, self).__init__()
         self.net = nn.Sequential(
             nn.Linear(in_channels, hidden_channels),
-            nn.ReLU(),
-            nn.Linear(hidden_channels, hidden_channels),
             nn.ReLU(),
             nn.Linear(hidden_channels, out_channels),
         )
@@ -43,69 +41,91 @@ class MLP_Head(nn.Module):
 
     
 class Critic(nn.Module):
-    def __init__(self, in_channels, hidden_channels=256):
+    def __init__(self, in_channels, hidden_channels=128):
         super().__init__()
         self.net = nn.Sequential(
+            nn.Conv2d(256, 1, kernel_size=1, stride=1),
+            nn.BatchNorm2d(1),
+            nn.ReLU(),
+            nn.Flatten(),
             nn.Linear(in_channels, hidden_channels),
             nn.ReLU(),
-            nn.Linear(hidden_channels, hidden_channels//4),
-            nn.ReLU(),
-            nn.Linear(hidden_channels//4, 1),
-            nn.Tanh()
+            nn.Linear(hidden_channels, 1),
         )
+        
     
     def forward(self, x):
         return self.net(x) 
     
     
 class Shobu_PPO(nn.Module):
-    def __init__(self, num_boards=1, board_size=8, dropout_rate=0.3):
+    def __init__(self, device, num_boards=1, board_size=8, dropout_rate=0.3):
         super(Shobu_PPO, self).__init__()
         
         # Input shape: (batch_size, num_boards, board_size, board_size)
         self.input_channels = num_boards
+        self.device = device
         
         self.backbone = nn.Sequential(
             # Block 1
-            nn.Conv2d(self.input_channels, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(self.input_channels, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            #ResidualBlock2D(16, 16),
 
             # Block 2
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(64, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
-            #ResidualBlock2D(32, 32),
-
-            # Block 3
-            nn.Conv2d(32, 64, kernel_size=3, stride=1),
-            nn.BatchNorm2d(64),
+            
+            # Residual blocks
+            ResidualBlock2D(256, 256),
+            ResidualBlock2D(256, 256),
+            #ResidualBlock2D(256, 256),
+            #ResidualBlock2D(256, 256),
+            #ResidualBlock2D(256, 256),
+            #ResidualBlock2D(256, 256),
+        )
+        
+        # passive head
+        self.passive_filter = nn.Sequential(
+            nn.Conv2d(256, 2, kernel_size=1, stride=1),
+            nn.BatchNorm2d(2),
             nn.ReLU(),
             nn.Flatten()
         )
-
         self.fc_input_size = self._calculate_fc_input_size(num_boards, board_size)
-        
-        # passive head
-        self.passive_head = MLP_Head(self.fc_input_size, 1024, hidden_channels=1024)
+        self.passive_pos_head = MLP_Head(self.fc_input_size, 64)
+        self.passive_dir_head = MLP_Head(self.fc_input_size+2, 8)
+        self.passive_dist_head = MLP_Head(self.fc_input_size+10, 2)
         # aggressive head (conditioned on first move)
-        self.aggressive_pos_head = MLP_Head(self.fc_input_size+1024, 64, hidden_channels=1024)
+        self.aggressive_pos_head = MLP_Head(self.fc_input_size+12, 64, hidden_channels=128)
         
         # critic head
-        self.critic = Critic(self.fc_input_size) 
+        self.critic = Critic(self.fc_input_size//2) 
         
 
     def _calculate_fc_input_size(self, num_boards, board_size):
         x = torch.randn(1, num_boards, board_size, board_size)
         x = self.backbone(x)
+        x = self.passive_filter(x)
         return x.view(1, -1).size(1)
+    
+    
+    def get_positional_encoding(self, piece_probs):
+        coords = torch.stack(torch.meshgrid(torch.arange(8, device=self.device), torch.arange(8, device=self.device)), dim=-1)
+        coords = coords.float().view(64, 2)
+
+        # Normalize coordinates to [0, 1]
+        coords = coords / 7.0
+
+        # Weight by piece_probs and return
+        return torch.matmul(piece_probs, coords)
     
     
     def get_features(self, x):
         # Input shape: (batch_size, num_boards, board_size, board_size)
         x = self.backbone(x)      
-        return x.view(x.size(0), -1)
+        return x
     
     
     def get_policy(self, x):
@@ -113,20 +133,31 @@ class Shobu_PPO(nn.Module):
         Get policy
         '''
         x = self.get_features(x)
+        x = self.passive_filter(x)
         
         # passive moves
-        passive_probs = self.passive_head(x)
+        passive_position_logits = self.passive_pos_head(x)
+        piece_probs = torch.softmax(passive_position_logits, dim=-1)
+        piece_pos_encoding = self.get_positional_encoding(piece_probs)
+        x = torch.concat((x,piece_pos_encoding), dim=1)
+        passive_dir_logits = self.passive_dir_head(x)
+        dir_onehot = F.one_hot(torch.argmax(passive_dir_logits, dim=-1), num_classes=8).float()
+        x = torch.concat((x,dir_onehot), dim=1)
+        passive_dist_logits = self.passive_dist_head(x)
         
         # aggressive moves, condition on first move
-        x = torch.concat((x,passive_probs), dim=1)
-        aggressive_position_probs = self.aggressive_pos_head(x)
+        dist_onehot = F.one_hot(torch.argmax(passive_dist_logits, dim=-1), num_classes=2).float()
+        x = torch.concat((x,dist_onehot), dim=1)
+        aggressive_position_logits = self.aggressive_pos_head(x)
         
         return {
             "passive": {
-                "position": passive_probs,
+                "position": passive_position_logits,
+                "direction": passive_dir_logits,
+                "distance": passive_dist_logits,
             },
             "aggressive": {
-                "position": aggressive_position_probs
+                "position": aggressive_position_logits
             }
         }
     
@@ -148,22 +179,32 @@ class Shobu_PPO(nn.Module):
         x = self.get_features(x)
         
         # passive moves
-        passive_probs = self.passive_head(x)
+        passive_position_logits = self.passive_pos_head(x)
+        piece_probs = torch.softmax(passive_position_logits, dim=-1)
+        piece_pos_encoding = self.get_positional_encoding(piece_probs)
+        x = torch.concat((x,piece_pos_encoding), dim=1)
+        passive_dir_logits = self.passive_dir_head(x)
+        dir_onehot = F.one_hot(torch.argmax(passive_dir_logits, dim=-1), num_classes=8).float()
+        x = torch.concat((x,dir_onehot), dim=1)
+        passive_dist_logits = self.passive_dist_head(x)
         
         # aggressive moves, condition on first move
-        x = torch.concat((x,passive_probs), dim=1)
-        aggressive_position_probs = self.aggressive_pos_head(x)
+        dist_onehot = F.one_hot(torch.argmax(passive_dist_logits, dim=-1), num_classes=2).float()
+        x = torch.concat((x,dist_onehot), dim=1)
+        aggressive_position_logits = self.aggressive_pos_head(x)
+        
         
         # critic
         q_value = self.critic(x)
         
         return {
             "passive": {
-                "position": passive_probs,
-
+                "position": passive_position_logits,
+                "direction": passive_dir_logits,
+                "distance": passive_dist_logits,
             },
             "aggressive": {
-                "position": aggressive_position_probs
+                "position": aggressive_position_logits
             },
             "q_value" : q_value
         }

rl_utils.py
@@ -122,8 +122,13 @@ def get_passive_logits(policy_output):
     '''
     # Extract passive probabilities
     p_pos = policy_output["passive"]["position"]
+    p_dir = policy_output["passive"]["direction"]
+    p_dist = policy_output["passive"]["distance"]
 
-    passive_logits = p_pos
+    # Create passive joint distribution tensor
+    passive_logits = p_pos.unsqueeze(-1).unsqueeze(-1) + p_dir.unsqueeze(1).unsqueeze(-1) + p_dist.unsqueeze(1).unsqueeze(1)
+    # Flatten the passive move probabilities
+    passive_logits = passive_logits.flatten(start_dim=1)
     return passive_logits
 
 
@@ -286,16 +291,20 @@ def intermediate_reward(state, step, max_step) -> torch.Tensor:
     # reward for piece discrepancy
     piece_discrep = torch.sum(state)
 
-    return (piece_discrep/16) #+ (-0.01)*(step/max_step)
+    return (-0.01)*(step) #+(piece_discrep/16)
 
     
 #### PLOTTING ####    
     
 def rolling_win_rate(win_list, window_size=25):
-    """Calculate rolling win rate over the last `window_size` episodes."""
+    """Calculate moving_average rolling win rate over the last `window_size` episodes."""
     if len(win_list) < window_size:
         return np.cumsum(win_list) / np.arange(1, len(win_list) + 1)
     return moving_average(win_list, window_size)
+
+def rolling_exp_win_rate(win_list, window_size=25):
+    """Calculate exponential_moving_average rolling win rate over the last `window_size` episodes."""
+    return exponential_moving_average(win_list, window_size)
 
 
 def moving_average(values, window_size=10):
@@ -303,7 +312,16 @@ def moving_average(values, window_size=10):
     return np.convolve(values, np.ones(window_size)/window_size, mode='valid')
 
 
-def plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, entropy_loss_list, win_list, opp_win_list, draw_list, plot_every: int, episode: int):
+def exponential_moving_average(values, alpha=0.05):
+    """Compute moving average to smooth the reward curve."""
+    ema = np.zeros_like(values, dtype=np.float64)
+    ema[0] = values[0]
+    for i in range(1, len(values)):
+        ema[i] = alpha * values[i] + (1 - alpha) * ema[i - 1]
+    return ema
+
+
+def plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, p_entropy_loss_list, a_entropy_loss_list, win_list, opp_win_list, draw_list, plot_every: int, episode: int):
     '''
     Plot training progress
     
@@ -323,9 +341,8 @@ def plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, entrop
     ax1.set_ylabel("Return", color="tab:blue")
     ax1.plot(reward_list, label="Raw Batch Return", alpha=0.4, color="tab:blue")
     
-    if len(reward_list) > plot_every:
-        smoothed_rewards = moving_average(reward_list, window_size=(plot_every))
-        ax1.plot(range(len(smoothed_rewards)), smoothed_rewards, label="Smoothed Batch Return", color="blue", linewidth=5)
+    smoothed_rewards = exponential_moving_average(reward_list)
+    ax1.plot(range(len(smoothed_rewards)), smoothed_rewards, label="Smoothed Batch Return", color="blue", linewidth=5)
 
     ax1.tick_params(axis="y", labelcolor="tab:blue")
 
@@ -334,9 +351,8 @@ def plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, entrop
     ax2.set_ylabel("PPO Loss", color="tab:orange")  
     ax2.plot(ppo_loss_list, label=" Raw Batch PPO Loss", alpha=0.2, color="tab:orange")
     
-    if len(ppo_loss_list) > plot_every:
-        smoothed_loss = moving_average(ppo_loss_list, window_size=(plot_every))
-        ax2.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch PPO Loss", color="orange", linewidth=5)
+    smoothed_loss = exponential_moving_average(ppo_loss_list)
+    ax2.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch PPO Loss", color="orange", linewidth=5)
 
     ax2.tick_params(axis="y", labelcolor="tab:orange")
     
@@ -346,46 +362,53 @@ def plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, entrop
     ax6.set_ylabel("Value Loss", color="tab:purple")  
     ax6.plot(value_loss_list, label=" Raw Batch Value Loss", alpha=0.2, color="tab:purple")
     
-    if len(value_loss_list) > plot_every:
-        smoothed_loss = moving_average(value_loss_list, window_size=(plot_every))
-        ax6.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch Value Loss", color="purple", linewidth=5)
+    smoothed_loss = exponential_moving_average(value_loss_list)
+    ax6.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch Value Loss", color="purple", linewidth=5)
 
     ax6.tick_params(axis="y", labelcolor="tab:purple")
     
     # Plot losses (right y-axis)
     ax7 = ax1.twinx()  
     ax7.spines["right"].set_position(("outward", 80)) 
-    ax7.set_ylabel("Entropy Loss", color="tab:green")  
-    ax7.plot(entropy_loss_list, label=" Raw Batch Entropy Loss", alpha=0.2, color="tab:green")
+    ax7.set_ylabel("Passive Entropy Loss", color="tab:green")  
+    ax7.plot(p_entropy_loss_list, label=" Raw Batch Passive Entropy Loss", alpha=0.2, color="tab:green")
     
-    if len(entropy_loss_list) > plot_every:
-        smoothed_loss = moving_average(entropy_loss_list, window_size=(plot_every))
-        ax7.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch Entropy Loss", color="green", linewidth=5)
+    smoothed_loss = exponential_moving_average(p_entropy_loss_list)
+    ax7.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch Passive Entropy Loss", color="green", linewidth=5)
 
     ax7.tick_params(axis="y", labelcolor="tab:green")
     
     # Plot losses (right y-axis)
     ax8 = ax1.twinx()  
     ax8.spines["right"].set_position(("outward", 120)) 
-    ax8.set_ylabel("Loss", color="tab:red")  
-    ax8.plot(loss_list, label=" Raw Batch Loss", alpha=0.4, color="tab:red")
+    ax8.set_ylabel("Aggressive Entropy Loss", color="tab:pink")  
+    ax8.plot(a_entropy_loss_list, label=" Raw Batch Aggressive Entropy Loss", alpha=0.2, color="tab:pink")
     
-    if len(entropy_loss_list) > plot_every:
-        smoothed_loss = moving_average(loss_list, window_size=(plot_every))
-        ax8.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch Loss", color="red", linewidth=5)
+    smoothed_loss = exponential_moving_average(a_entropy_loss_list)
+    ax8.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch Aggressive Entropy Loss", color="pink", linewidth=5)
 
-    ax8.tick_params(axis="y", labelcolor="tab:red")
+    ax8.tick_params(axis="y", labelcolor="tab:pink")
+    
+    # Plot losses (right y-axis)
+    ax9 = ax1.twinx()  
+    ax9.spines["right"].set_position(("outward", 160)) 
+    ax9.set_ylabel("Loss", color="tab:red")  
+    ax9.plot(loss_list, label=" Raw Batch Loss", alpha=0.4, color="tab:red")
+    
+    smoothed_loss = exponential_moving_average(loss_list)
+    ax9.plot(range(len(smoothed_loss)), smoothed_loss, label="Smoothed Batch Loss", color="red", linewidth=5)
+
+    ax9.tick_params(axis="y", labelcolor="tab:red")
 
     # Plot win rate for model
     fig2, ax3 = plt.subplots(figsize=(12, 8))
     ax3.set_xlabel("Episode")
     ax3.spines["right"].set_position(("outward", 60))  # Move win rate axis outward
-    ax3.set_ylabel(f"Rolling Win Rate (Last {plot_every})", color="tab:green")
+    ax3.set_ylabel(f"Rolling Win Rate)", color="tab:green")
 
     # Calculate and plot rolling win rate
-    if len(win_list) > plot_every:
-        rolling_rate = rolling_win_rate(win_list, window_size=(plot_every))
-        ax3.plot(range(len(rolling_rate)), rolling_rate, alpha=0.2,label="Rolling Win Rate", color="green", linewidth=3)
+    rolling_rate = exponential_moving_average(win_list)
+    ax3.plot(range(len(rolling_rate)), rolling_rate, alpha=0.2,label="Rolling Win Rate", color="green", linewidth=3)
 
     ax3.tick_params(axis="y", labelcolor="tab:green")
     ax3.set_ylim(0, 1)
@@ -394,12 +417,11 @@ def plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, entrop
     # Plot draw rate
     ax4 = ax3.twinx()
     ax4.spines["right"]  # Move win rate axis outward
-    ax4.set_ylabel(f"Rolling Draw Rate (Last {plot_every})", color="tab:purple")
+    ax4.set_ylabel(f"Rolling Draw Rate)", color="tab:purple")
 
     # Calculate and plot rolling draw rate
-    if len(draw_list) > plot_every:
-        rolling_drawrate = rolling_win_rate(draw_list, window_size=(plot_every))
-        ax4.plot(range(len(rolling_drawrate)), rolling_drawrate, alpha=0.2,label="Rolling Draw Rate", color="purple", linewidth=3)
+    rolling_drawrate = exponential_moving_average(draw_list)
+    ax4.plot(range(len(rolling_drawrate)), rolling_drawrate, alpha=0.2,label="Rolling Draw Rate", color="purple", linewidth=3)
 
     ax4.tick_params(axis="y", labelcolor="tab:purple")
     ax4.set_ylim(0, 1)
@@ -407,18 +429,17 @@ def plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, entrop
     # Plot win rate for opp
     ax5 = ax3.twinx()
     ax5.spines["right"].set_position(("outward", 60))  # Move win rate axis outward
-    ax5.set_ylabel(f"Rolling Opponent Win Rate (Last {plot_every})", color="tab:cyan")
+    ax5.set_ylabel(f"Rolling Opponent Win Rate)", color="tab:cyan")
 
     # Calculate and plot rolling draw rate
-    if len(draw_list) > plot_every:
-        rolling_opprate = rolling_win_rate(opp_win_list, window_size=(plot_every))
-        ax5.plot(range(len(rolling_opprate)), rolling_opprate, alpha=0.2,label="Rolling Opponent Win Rate", color="cyan", linewidth=3)
+    rolling_opprate = exponential_moving_average(opp_win_list)
+    ax5.plot(range(len(rolling_opprate)), rolling_opprate, alpha=0.2,label="Rolling Opponent Win Rate", color="cyan", linewidth=3)
 
     ax5.tick_params(axis="y", labelcolor="tab:cyan")
     ax5.set_ylim(0, 1)
 
 
-    plt.title(f"Training Progress (Episode {episode+1})")
+    plt.title(f"Training Progress Episode {episode+1} (Displaying Last 2000 Episodes)")
     fig.tight_layout()  # Adjust layout for better spacing
     plt.grid()
     plt.show()

shobu_rl.py
@@ -1,6 +1,7 @@
 from shobu import *
 from rl_utils import *
 from models import *
+from agent import RandomAgent
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,9 +14,9 @@ from itertools import combinations
 
 # hyperparameters
 # training parameters
-MAX_TURNS = 80
+MAX_TURNS = 100
 MAX_GAMES = 500000
-BATCH_SIZE = 512
+BATCH_SIZE = 256
 EPOCHS = 3
 ON_POLICY = 50
 
@@ -24,6 +25,7 @@ ON_POLICY = 50
 C1 = 0.5
 # Entropy loss weight
 C2 = 0.05
+C3 = 0.01
 # Clipped PPO parameter
 EPSILON = 0.2
 # Discount factor parameters
@@ -32,24 +34,21 @@ LAMBDA = 0.95
 # size of replay buffer
 MEMORY_SIZE = 500000
 # win reward
-WIN_REWARD = 10
+WIN_REWARD = 1
 # add new opponent every UPDATE_OPPS episodes
-UPDATE_OPPS = 1000
+UPDATE_OPPS = 5000
 # max number of opponents to track at a given time
-OPP_QUEUE_LENGTH = 6
+OPP_QUEUE_LENGTH = 8
 device = torch.device(
     "cuda" if torch.cuda.is_available() else
     "cpu"
 )
-
 
 class Shobu_RL():
     def __init__(self, ppo_model: nn.Module):
         super().__init__()
         # init models
         self.set_model(ppo_model)
-        # steps throughout training
-        self.train_steps = 0
         # init game
         self.init_game()
         
@@ -79,10 +78,6 @@ class Shobu_RL():
         self.ppo_model = ppo_model
         # opponents - periodically freeze value model and add to list
         self.opponent_pool = deque([], maxlen=OPP_QUEUE_LENGTH)
-        opponent = copy.deepcopy(ppo_model) 
-        for param in opponent.parameters():
-            param.requires_grad = False
-        self.opponent_pool.append(opponent)
 
         
     def remove_opponent(self):
@@ -110,31 +105,26 @@ class Shobu_RL():
         '''
         Select policy from model for a given board state
         '''
-        # train
+        # if train, count steps
         if train:
-            # PPO moves
-            if self.cur_turn == self.model_player:
-                self.train_steps += 1
-                self.steps_done += 1 
-                with torch.no_grad():
-                    policy_output = self.ppo_model.get_policy(state)
-                    move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask = model_action(policy_output, self.board, device)
-                return move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask
-            # opp network moves
-            else:
-                with torch.no_grad():
-                    policy_output = self.opp.get_policy(state)
-                    move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask = model_action(policy_output, self.board, device)
-                return move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask
+            self.steps_done += 1 
+        with torch.no_grad():
+            policy_output = self.ppo_model.get_policy(state)
+            move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask = model_action(policy_output, self.board, device)
+        return move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask
         
-        # inference
+        
+    def select_opponent_action(self, board, device):
+        if type(self.opp) is RandomAgent:
+            move = self.opp.move(board)
         else:
-            self.steps_done += 0.5
+            board_state = board.as_matrix()
+            start_state = torch.tensor(board_state.copy(), device=device, dtype=torch.float32).unsqueeze(0)
             with torch.no_grad():
-                policy_output = self.ppo_model.get_policy(state)
-                move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask = model_action(policy_output, self.board, device)
-            return move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask
-
+                policy_output = self.opp.get_policy(start_state)
+                move, _, _, _, _, _, _ = model_action(policy_output, self.board, device)
+        return move
+    
     
     def model_play_game(self, memory, train=True):
         '''
@@ -142,23 +132,26 @@ class Shobu_RL():
         '''
         # select opponents
         if train:
-            self.opp = random.choice(self.opponent_pool)
+            if random.uniform(0, 1) < (1/(len(self.opponent_pool)+1)):
+                self.opp = RandomAgent()
+            else:
+                self.opp = random.choice(self.opponent_pool)
           
         while (self.steps_done < MAX_TURNS):  
             if not train:
                 print("Current board")
                 print(self.board)
                 
-            board_state = self.board.as_matrix()
-            
-            # get input move by sampling from policy model
-            start_state = torch.tensor(board_state.copy(), device=device, dtype=torch.float32).unsqueeze(0)
-            move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask = self.select_action(start_state, device, train)
-            
-            # push to memory if player is the trainable model
             if self.cur_turn == self.model_player:
-                memory.push(start_state, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask, 0, 0)
-                        
+                board_state = self.board.as_matrix()
+                start_state = torch.tensor(board_state.copy(), device=device, dtype=torch.float32).unsqueeze(0)
+                move, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask = self.select_action(start_state, device)
+                # push to memory if player is the trainable model
+                if self.cur_turn == self.model_player:
+                    memory.push(start_state, passive_index, aggressive_index, passive_probs, aggressive_probs, passive_mask, aggressive_mask, 0, 0)
+            else:
+                move = self.select_opponent_action(self.board, device)
+                   
             # apply move
             self.board = self.board.apply(move)
             
@@ -190,14 +183,14 @@ class Shobu_RL():
         loss_list = []
         ppo_loss_list = []
         value_loss_list = []
-        entropy_loss_list = []
+        p_entropy_loss_list = []
+        a_entropy_loss_list = []
         reward_list = []
         win_list = []
         draw_list = []
         opp_win_list = []
         # memory for caching states, action, next state, and reward
         train_memory = ReplayMemory(capacity=MEMORY_SIZE)
-        self.ppo_model.train()
         
         for episode in range(MAX_GAMES):
             # init a new game
@@ -206,8 +199,10 @@ class Shobu_RL():
             
             # play game
             t0 = time.time()
+            self.ppo_model.eval()
             episode_memory = ReplayMemory(capacity=MEMORY_SIZE)
             episode_memory = self.model_play_game(episode_memory)
+            self.ppo_model.train()
             t1 = time.time()
             print(f"\n Game simulated in: {t1-t0}")
             
@@ -233,7 +228,7 @@ class Shobu_RL():
                 else:
                     rewards[-1] += -WIN_REWARD*1.5
                 # compute returns + advantages
-                returns, advantages = compute_returns(q_values, rewards, device, GAMMA, LAMBDA)
+                returns, advantages = compute_returns(rewards=rewards, values=q_values, device=device, gamma=GAMMA, lam=LAMBDA)
             # add advantage and returns to queue of transitions
             transitions_with_advantage = []
             for i in range(len(episode_memory.memory)):
@@ -250,12 +245,10 @@ class Shobu_RL():
             # training step every ON_POLICY episodes
             if ((episode+1) % ON_POLICY) == 0:
                 # normalize returns and advantages
-                returns = torch.stack([t.returns for t in train_memory.memory])
                 advantages = torch.stack([t.advantages for t in train_memory.memory])
                 advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
-                returns = (returns - returns.mean()) / (returns.std() + 1e-8)
                 for i, t in enumerate(train_memory.memory):
-                    train_memory.memory[i] = Transition(t.state, t.passive, t.aggressive, t.passive_probs, t.aggressive_probs, t.passive_mask, t.aggressive_mask, advantages[i], returns[i])
+                    train_memory.memory[i] = Transition(t.state, t.passive, t.aggressive, t.passive_probs, t.aggressive_probs, t.passive_mask, t.aggressive_mask, advantages[i], t.returns)
                     
                 # train for EPOCHS epochs
                 for e in range(EPOCHS):
@@ -264,7 +257,8 @@ class Shobu_RL():
                     train_memory_copy.shuffle()
                     batch_ppo_loss_list = []
                     batch_value_loss_list = []
-                    batch_entropy_loss_list = []
+                    batch_p_entropy_loss_list = []
+                    batch_a_entropy_loss_list = []
                     batch_loss = []
                     batch_reward = []
                     while len(train_memory_copy) > 0:
@@ -300,29 +294,31 @@ class Shobu_RL():
                         policy_loss = -torch.min(ratio * advantages, clipped_ratio * advantages).mean()
 
                         # value function loss
-                        value_loss = F.huber_loss(self.ppo_model.value_function(state_batch).squeeze(), returns)
+                        values = self.ppo_model.value_function(state_batch).squeeze()
+                        value_loss = F.huber_loss(values, returns)
 
                         # entropy loss
                         passive_logits[passive_masks == 0] = -1e10
                         aggressive_logits[aggressive_masks == 0] = -1e10
                         entropy_passive = torch.distributions.Categorical(logits=passive_logits).entropy()
                         entropy_aggressive = torch.distributions.Categorical(logits=aggressive_logits).entropy()
-                        entropy_loss = -torch.mean(entropy_passive + entropy_aggressive) 
+                        passive_entropy_loss = - (entropy_passive.mean())
+                        aggressive_entropy_loss = - (entropy_aggressive.mean())
 
                         # Final PPO loss
-                        total_loss = policy_loss + (C1*value_loss) + (C2*entropy_loss)
-                        #print(policy_loss, value_loss, entropy_loss)
+                        total_loss = policy_loss + (C1*value_loss) + C2 * (passive_entropy_loss) + C3 * (aggressive_entropy_loss)
 
                         # backprop and optimize
                         opt.zero_grad()
                         total_loss.backward()
-                        torch.nn.utils.clip_grad_norm_(self.ppo_model.parameters(), max_norm=0.5)
+                        total_norm = torch.nn.utils.clip_grad_norm_(self.ppo_model.parameters(), max_norm=0.5)
                         opt.step()
 
                         # metrics
                         batch_ppo_loss_list.append(policy_loss.item())
                         batch_value_loss_list.append(value_loss.item())
-                        batch_entropy_loss_list.append(entropy_loss.item())
+                        batch_p_entropy_loss_list.append(passive_entropy_loss.item())
+                        batch_a_entropy_loss_list.append(aggressive_entropy_loss.item())
                         batch_loss.append(total_loss.item())
                         batch_reward.append(np.mean(returns.clone().cpu().numpy()))
                         
@@ -330,12 +326,14 @@ class Shobu_RL():
                     # update metrics
                     avg_epoch_ppo_loss = np.mean(batch_ppo_loss_list)
                     avg_epoch_value_loss = np.mean(batch_value_loss_list)
-                    avg_epoch_entropy_loss = np.mean(batch_entropy_loss_list)
+                    avg_epoch_p_entropy_loss = np.mean(batch_p_entropy_loss_list)
+                    avg_epoch_a_entropy_loss = np.mean(batch_a_entropy_loss_list)
                     avg_epoch_loss = np.mean(batch_loss)
                     avg_epoch_reward = np.mean(batch_reward)
                     ppo_loss_list.append(avg_epoch_ppo_loss)
                     value_loss_list.append(avg_epoch_value_loss)
-                    entropy_loss_list.append(avg_epoch_entropy_loss)
+                    p_entropy_loss_list.append(avg_epoch_p_entropy_loss)
+                    a_entropy_loss_list.append(avg_epoch_a_entropy_loss)
                     loss_list.append(avg_epoch_loss)
                     reward_list.append(avg_epoch_reward)
 
@@ -357,7 +355,15 @@ class Shobu_RL():
             plot_every = ON_POLICY
             if ((episode+1) % plot_every) == 0:
                 # plot progress
-                plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, entropy_loss_list, win_list, opp_win_list, draw_list, plot_every, episode)
+                plot_progress(reward_list, loss_list, ppo_loss_list, value_loss_list, p_entropy_loss_list, a_entropy_loss_list, win_list, opp_win_list, draw_list, plot_every, episode)
+                print("Top passive action prob:", torch.softmax(passive_logits, dim=-1).max().item())
+                print("Top aggressive action prob:", torch.softmax(aggressive_logits, dim=-1).max().item())
+                print("Avg valid passive moves:", (passive_masks == 1).float().mean().item() * passive_masks.shape[-1])
+                print("Avg valid aggressive moves:", (aggressive_masks == 1).float().mean().item() * aggressive_masks.shape[-1])
+                print("Entropies (p and a):" , avg_epoch_p_entropy_loss, avg_epoch_a_entropy_loss)
+                print("Passive action prob:", torch.softmax(passive_logits, dim=-1)[0])
+                print("Aggressive action prob:", torch.softmax(aggressive_logits, dim=-1)[0])
+                print(f"Values: mean={values.mean().item():.3f}, std={values.std().item():.3f}")
 
             # save checkpoints
             if ((episode+1)%1000) == 0: