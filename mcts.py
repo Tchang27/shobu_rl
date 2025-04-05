5030a4bc7737034ece158a021feaec85893a319d
Thomas Chang <seowo1@MacBook-Pro-771.local>
2025-04-05 01:37:13 -0400

    simultaneous simualtion and train

--- mcts.py
+++ mcts.py
@@ -2,7 +2,7 @@ import cProfile
 import copy
 from collections import deque
 from datetime import datetime
-from torch.multiprocessing import Pool, Queue
+from torch.multiprocessing import Pool, set_sharing_strategy, set_start_method, Queue, Process
 from rl_utils import ReplayMemory_MCTS, Transition_MCTS
 import numpy as np
 import torch
@@ -11,10 +11,16 @@ import torch.nn.functional as F
 import torch.nn as nn
 from rl_utils import *
 from models import Shobu_MCTS, HISTORY_SIZE
-
+import gc
+import psutil
 from shobu import ShobuMove, Shobu, Player
 
-MAX_GAME_LEN = 128
+MAX_GAME_LEN = 5
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "cpu"
+)
+
 
 class MCNode:
     def __init__(self, prior: float, player: Player):
@@ -46,19 +52,6 @@ class MCNode:
         Computes argmax_a (Q(s,a) + u(s,a)). See wikipedia link for "selection"
         and "expansion" terminology definitions
         """
-#         max_a = -np.inf
-#         a_move, a_t = None, None
-#         for move, child in self.children.items():
-#             # Inlining ucb calculation to avoid function call overhead
-#             # exploitation bonus
-#             q = child.num_visits and -child.total_reward / child.num_visits
-#             # exploration bonus
-#             u = child.prior * np.sqrt(self.num_visits) / (1 + child.num_visits)
-#             ucb = q + u
-#             if (score := ucb) > max_a:
-#                 max_a = score
-#                 a_move, a_t = move, child
-#         return a_move, a_t
         moves = list(self.children.keys())
         children = list(self.children.values())
 
@@ -115,9 +108,14 @@ class MCTree:
         self.root.state = starting_state
         self.model = model
         self.device = device
+        
+    def __del__(self):
+        self.root = None
+        # Explicitly delete large objects
+        if hasattr(self, 'model'):
+            del self.model
 
     def _value_and_policy(self, path: list[MCNode], noise=True) -> tuple[float, dict[ShobuMove, torch.tensor]]:
-        torch.set_num_threads(1)
         recent_history = path[-HISTORY_SIZE:] # not necessarily 8 elts long!!
 #         past_boards = []
 #         for node in recent_history:
@@ -186,218 +184,251 @@ class MCTree:
             else:
                 node.total_reward -= evaluation
 
+                
+def temperature_scheduler(epoch_no, move_no):
+    """
+    this is just a straight up guess, can try something diff too
+    but generally temperature should decrease over time
+    """
+    if move_no < 30:
+        return 1
+    elif move_no > 60:
+        return 0
+    else:
+        return (60 - move_no) / 30
 
-def pickled_play_game(smr: tuple[int, int, "Shobu_MCTS_RL"]) -> ReplayMemory_MCTS:
-    episode, game_num = smr[0], smr[1]
     
-    seed = episode * 1000 + game_num
-    np.random.seed(seed)
-    torch.manual_seed(seed)
+def play_game(model, device, memory: ReplayMemory_MCTS, epoch: int):
+    # to avoid file descriptor issues
+    set_sharing_strategy('file_system')
     
-    memory = ReplayMemory_MCTS()
-    smr[2].play_game(memory, episode)
-    return memory
+    board = Shobu.starting_position()
+    generated_training_data = []
+    num_moves = 0
+    game_end_reward = None
+    while True:
 
+        ######
+        # PERFORMANCE PROFILING
+        ###
+        # pr = cProfile.Profile()
+        # pr.enable()
+        mcts = MCTree(model, board, device)
+        # playout randomization
+        full_search = False
+        if np.random.random() < 0.75:
+            rollout = mcts.search(100, noise=False)
+        else:
+            rollout = mcts.search(400, noise=True)
+            full_search = True
+        _sum_pi = sum([child.num_visits for child in rollout.children.values()])
 
-GAMES_PER_EPOCH = 32 # TODO tune this
+        # This is what policy should learn
+        pi = {}
+        for k in rollout.children.keys():
+            pi[k] = rollout.children[k].num_visits / _sum_pi
+        # Only append full search
+        if full_search:
+            generated_training_data.append((board, pi))
+
+        # choose the next move based on tree search results
+        tau = temperature_scheduler(epoch, num_moves)
+        selected_move = rollout.sample_move(tau)
+        board = board.apply(selected_move)
+
+        # Check winner
+        if (winner := board.check_winner()) is not None:
+            assert winner == Player.BLACK
+            game_end_reward = 1
+            break
+        elif num_moves >= MAX_GAME_LEN:
+            game_end_reward = 0
+            break
+
+        # next player also plays black
+        board.flip()
+        num_moves += 1
+
+        ###
+        # END PROFILING
+        ######
+        # pr.disable()
+        # pr.dump_stats(datetime.today().strftime('%Y-%m-%d-%H:%M:%S') + f"-epoch:{epoch}-move:{num_moves}.dmp")
+
+    # last element of generated_training_data, generated_training_data[-1],
+    # is the game state right before the winning move. so that "player"
+    # has game reward 1. the reward for preceding board states thus alternates
+    # between -1 and 1. This "label" of -1 or 1 will be used to train the
+    # value net
+    generated_training_data_with_rewards = []
+    for board, pi in reversed(generated_training_data):
+        generated_training_data_with_rewards.append((board, pi, game_end_reward))
+        game_end_reward *= -1
+
+    # Push entire game history into ReplayMemory
+    history = deque()
+    for board, pi, reward in generated_training_data_with_rewards:
+        history.append(board.as_matrix())
+        padded = [np.zeros((8,4,4)) for _ in range(HISTORY_SIZE-len(history))] + list(history)
+        state_tensor = torch.tensor(np.concatenate(padded), device=device, dtype=torch.float32).unsqueeze(0)
+        memory.push(board, state_tensor, reward, pi)
+
+        if len(history) >= HISTORY_SIZE:
+            history.popleft()
+            
+            
+GAMES_PER_EPOCH = 96 # TODO tune this
 MAX_GAMES = 10000
 EPOCHS = 15 # number of epochs to train per game
 MINIBATCH_SIZE = 256
-POOL_SIZE = 32
+
+POOL_SIZE = 16
 WINDOW_SIZE = 50000 # TODO tune this
+WARMUP = 5000
+            
+def pickled_play_game(model_state_dict, buffer, seed):
+    torch.set_num_threads(1)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    local_model = Shobu_MCTS(device)
+    local_model.load_state_dict(model_state_dict)
+    episode = 0
+    while True:
+        memory = ReplayMemory_MCTS()
+        play_game(local_model, device, memory, episode)
+        for transition in memory.memory:
+            if buffer.qsize() > WINDOW_SIZE:
+                buffer.get_nowait()
+            buffer.put(transition)
+        episode += 1     
+        
+
 class Shobu_MCTS_RL:
     # init
-    def __init__(self, model: Shobu_MCTS, device: torch.device):
+    def __init__(self, device: torch.device):
         super().__init__()
-        # init models
-        self.model = model
         # parameters
-        self.epochs = 50000
-        self.minibatch_size = MINIBATCH_SIZE
-        self.device = device
-
-    def temperature_scheduler(self, epoch_no, move_no):
-        """
-        this is just a straight up guess, can try something diff too
-        but generally temperature should decrease over time
-        """
-        if move_no < 30:
-            return 1
-        elif move_no > 60:
-            return 0
-        else:
-            return (60 - move_no) / 30
-
-    def play_game(self, memory: ReplayMemory_MCTS, epoch: int):
-        board = Shobu.starting_position()
-        generated_training_data = []
-        num_moves = 0
-        game_end_reward = None
-        while True:
-
-            ######
-            # PERFORMANCE PROFILING
-            ###
-            # pr = cProfile.Profile()
-            # pr.enable()
-
-            mcts = MCTree(self.model, board, self.device)
-            # playout randomization
-            full_search = False
-            if np.random.random() < 0.75:
-                rollout = mcts.search(100, noise=False)
-            else:
-                rollout = mcts.search(400, noise=True)
-                full_search = True
-            _sum_pi = sum([child.num_visits for child in rollout.children.values()])
-
-            # This is what policy should learn
-            pi = {}
-            for k in rollout.children.keys():
-                pi[k] = rollout.children[k].num_visits / _sum_pi
-            # Only append full search
-            if full_search:
-                generated_training_data.append((board, pi))
-
-            # choose the next move based on tree search results
-            tau = self.temperature_scheduler(epoch, num_moves)
-            selected_move = rollout.sample_move(tau)
-            board = board.apply(selected_move)
-
-            # Check winner
-            if (winner := board.check_winner()) is not None:
-                assert winner == Player.BLACK
-                game_end_reward = 1
-                break
-            elif num_moves >= MAX_GAME_LEN:
-                game_end_reward = 0
-                break
-
-            # next player also plays black
-            board.flip()
-            num_moves += 1
-            t1 = time.time()
-            # print(t1 - t0)
-
-            ###
-            # END PROFILING
-            ######
-            # pr.disable()
-            # pr.dump_stats(datetime.today().strftime('%Y-%m-%d-%H:%M:%S') + f"-epoch:{epoch}-move:{num_moves}.dmp")
-
-		# last element of generated_training_data, generated_training_data[-1],
-        # is the game state right before the winning move. so that "player"
-        # has game reward 1. the reward for preceding board states thus alternates
-        # between -1 and 1. This "label" of -1 or 1 will be used to train the
-        # value net
-        generated_training_data_with_rewards = []
-        for board, pi in reversed(generated_training_data):
-            generated_training_data_with_rewards.append((board, pi, game_end_reward))
-            game_end_reward *= -1
-
-        # Push entire game history into ReplayMemory
-        history = deque()
-        for board, pi, reward in generated_training_data_with_rewards:
-            history.append(board.as_matrix())
-            padded = [np.zeros((8,4,4)) for _ in range(HISTORY_SIZE-len(history))] + list(history)
-            state_tensor = torch.tensor(np.concatenate(padded), device=self.device, dtype=torch.float32).unsqueeze(0)
-            memory.push(board, state_tensor, reward, pi)
-
-            if len(history) >= HISTORY_SIZE:
-                history.popleft()
-                
-    # train
-    def train(self, opt: torch.optim.Optimizer, scheduler: torch.optim.lr_scheduler._LRScheduler):
-        self.model.train()
+        self.device = device                
+    
+    
+    def train_loop(self, model, buffer):
+        # metrics
         loss_list = []
         policy_loss_list = []
         value_loss_list = []
         reward_list = []
         
-        rolling_window = ReplayMemory_MCTS(capacity=WINDOW_SIZE)
-        with Pool(POOL_SIZE) as p:
-            for episode in range(MAX_GAMES):
-                print(f"Simulating games...")
-                t0 = time.time()
-                memories = p.map(pickled_play_game, [(episode, game_num, self) for game_num in range(GAMES_PER_EPOCH)])
-                t1 = time.time()
-                print(f"Time for all games: {t1 - t0}")
+        # optimizer
+        critic_params = list(model.critic.parameters())
+        backbone_params = list(model.backbone.parameters())
+        actor_params = [
+            p for p in model.parameters() 
+            if (not any(p is cp for cp in critic_params)) and (not any(p is bp for bp in backbone_params))
+        ]  # All other params (policy heads)
+        opt = torch.optim.Adam([
+            {'params': actor_params, 'lr': 2e-5},
+            {'params': backbone_params, 'lr': 2e-5},
+            {'params': critic_params, 'lr': 2e-5}
+        ], amsgrad=True, weight_decay=3e-5)
+        scheduler = torch.optim.lr_scheduler.StepLR(opt, step_size=1000, gamma=0.5)
+        
+        # warm up period
+        batch_size = MINIBATCH_SIZE
+        while buffer.qsize() < WARMUP:
+            print(f"Warming up... ({buffer.qsize()})")
+            time.sleep(1)
+        
+        # train step
+        epoch = 0
+        while True:
+            print(buffer.qsize())
+            t0 = time.time()
+            items = []
+            while not buffer.empty():
+                items.append(buffer.get_nowait())
+            for item in items:
+                buffer.put_nowait(item)
+            # Randomly sample a batch of items
+            minibatch = random.sample(items, min(batch_size, len(items)))
+            batch = Transition_MCTS(*zip(*minibatch))
+            boards = batch.board
+            states = torch.concatenate(batch.state)
+            rewards = torch.tensor(np.stack(batch.reward), device=self.device, dtype=torch.float32)
+            mcts_dist = np.stack(batch.mcts_dist)
 
-                # update memory
-                for m in memories:
-                    rolling_window.extend(m)
+            ### value loss ###
+            values = model.get_value(states).squeeze()
+            value_loss = F.mse_loss(values, rewards)
 
-                # TODO: consider
-                # memory.augment()
-    
-                t0 = time.time()
-                print("Training on simulations...")
-                batch_policy_loss_list = []
-                batch_value_loss_list = []
-                batch_loss = []
-                batch_reward = []
-                for epoch in range(EPOCHS):
-                    transitions = rolling_window.sample(self.minibatch_size)
-                    batch = Transition_MCTS(*zip(*transitions))
-                    boards = batch.board
-                    states = torch.concatenate(batch.state)
-                    rewards = torch.tensor(np.stack(batch.reward), device=self.device, dtype=torch.float32)
-                    mcts_dist = np.stack(batch.mcts_dist)
+            ### policy loss ###
+            # get distributions
+            policy_losses = []
+            for state, board, pi_dict in zip(states, boards, mcts_dist):
+                policy_outputs = model.get_policy(state.unsqueeze(0))  # Get policy logits
+                move_to_logit = get_joint_logits(board, policy_outputs, logits=True)
+                policy = torch.tensor([move_to_logit[k] for k in move_to_logit.keys()], device=self.device, dtype=torch.float32)
+                pi_dist = torch.tensor([pi_dict[k] for k in pi_dict.keys()], device=self.device, dtype=torch.float32)
+                policy_losses.append(F.kl_div(F.log_softmax(policy, dim=-1), pi_dist, reduction="sum"))
+            policy_loss = torch.mean(torch.stack(policy_losses))
 
-                    ### value loss ###
-                    values = self.model.get_value(states).squeeze()
-                    value_loss = F.mse_loss(values, rewards)
+            ### optimize ###
+            loss = value_loss + policy_loss
+            opt.zero_grad()
+            loss.backward()
+            # can adjust this or remove entirely
+            total_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            opt.step()
+            scheduler.step()
+            print(f"Trained on batch, loss = {loss:.4f}")
 
-                    ### policy loss ###
+            ### metrics ###
+            policy_loss_list.append(policy_loss.item())
+            value_loss_list.append(value_loss.item())
+            loss_list.append(loss.item())
+            reward_list.append(np.mean(rewards.clone().cpu().numpy()))
 
-                    # get distributions
-                    policy_losses = []
-                    for state, board, pi_dict in zip(states, boards, mcts_dist):
-                        policy_outputs = self.model.get_policy(state.unsqueeze(0))  # Get policy logits
-                        move_to_logit = get_joint_logits(board, policy_outputs, logits=True)
-                        policy = torch.tensor([move_to_logit[k] for k in move_to_logit.keys()], device=self.device, dtype=torch.float32)
-                        pi_dist = torch.tensor([pi_dict[k] for k in pi_dict.keys()], device=self.device, dtype=torch.float32)
-                        policy_losses.append(F.kl_div(F.log_softmax(policy, dim=-1), pi_dist, reduction="sum"))
-                    policy_loss = torch.mean(torch.stack(policy_losses))
 
-                    ### optimize ###
-                    loss = value_loss + policy_loss
-                    opt.zero_grad()
-                    loss.backward()
-                    # can adjust this or remove entirely
-                    total_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=5.0)
-                    print(total_norm)
-                    opt.step()
-                    scheduler.step()
+            ### update plot ###
+            t1 = time.time()
+            plot_progress_MCTS(reward_list, loss_list, policy_loss_list, value_loss_list, epoch)
+            print(f"Train step time: {t1-t0}")
 
-                    ### metrics ###
-                    batch_policy_loss_list.append(policy_loss.item())
-                    batch_value_loss_list.append(value_loss.item())
-                    batch_loss.append(loss.item())
-                    batch_reward.append(np.mean(rewards.clone().cpu().numpy()))
+            # save checkpoints
+            if ((epoch+1)%500) == 0:
+                torch.save(model.state_dict(), f'mcts_checkpoints/mcts_checkpoint_{epoch+1}.pth')
+
+            # garbage collect
+            gc.collect()
+
+            # update epoch
+            epoch += 1
+
+        # save final model
+        torch.save(model.state_dict(), 'mcts_checkpoints/mcts_final.pth')
             
-                ### update metrics ###
-                avg_epoch_policyloss = np.mean(batch_policy_loss_list)
-                avg_epoch_value_loss = np.mean(batch_value_loss_list)
-                avg_epoch_loss = np.mean(batch_loss)
-                avg_epoch_reward = np.mean(batch_reward)
-                policy_loss_list.append(avg_epoch_policyloss)
-                value_loss_list.append(avg_epoch_value_loss)
-                loss_list.append(avg_epoch_loss)
-                reward_list.append(avg_epoch_reward)
-
-    
-                ### update plot ###
-                t1 = time.time()
-                plot_progress_MCTS(reward_list, loss_list, policy_loss_list, value_loss_list, episode)
-                print(f"Training time: {t1-t0}")
                 
-                # save checkpoints
-                if ((episode+1)%10) == 0:
-                    torch.save(self.model.state_dict(), f'mcts_checkpoints/mcts_checkpoint_{episode+1}.pth')
+    # simultaneous simulation and training
+    def train(self):
+        # multiproc setting for torch
+        set_sharing_strategy('file_system')
+        set_start_method('spawn', force=True)
+        
+        # model
+        model = Shobu_MCTS(device)
+        model.to(device)
+        
+        replay_buffer = Queue(maxsize=WINDOW_SIZE) 
 
-            # save final model
-            torch.save(self.model.state_dict(), 'mcts_checkpoints/mcts_final.pth')
+        workers = []
+        for i in range(POOL_SIZE):
+            p = Process(target=pickled_play_game, args=(model.state_dict(), replay_buffer, 42 + i))
+            p.start()
+            workers.append(p)
+
+        self.train_loop(model, replay_buffer)
+
+        for w in workers:
+            p.join()
     
 
 if __name__ == "__main__":
